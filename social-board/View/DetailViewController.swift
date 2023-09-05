6b18bf83f45a20654743abb3dee2da69b24a50c6
jisulee154@gmail.com <127957826+jisulee154@users.noreply.github.com>
2023-09-05 19:00:14 +0900

    상세화면 rightBarButtonItem 설정

--- social-board/View/DetailViewController.swift
+++ social-board/View/DetailViewController.swift
@@ -17,6 +17,36 @@ class DetailViewController: UIViewController {
     }
     
     func configureNavigationBarItem () {
+        //MARK: - Right BarItem 설정
+        let shareBtn = UIButton()
+        let shareBtnImage = UIImage(systemName: "square.and.arrow.up")
+        shareBtn.setImage(shareBtnImage, for: .normal)
+//        shareBtn.addTarget(self, action: #selector(), for: .touchUpInside)
+        shareBtn.tintColor = .black
         
+        let likeBtn = UIButton()
+        let likeBtnImage = UIImage(systemName: "heart")
+        likeBtn.setImage(likeBtnImage, for: .normal)
+        likeBtn.tintColor = .black
+        
+        let reportBtn = UIButton()
+        let reportBtnImage = UIImage(systemName: "ellipsis")
+        reportBtn.setImage(reportBtnImage, for: .normal)
+        reportBtn.tintColor = .black
+        
+        let rightStack = UIStackView(arrangedSubviews: [shareBtn, likeBtn, reportBtn])
+        rightStack.axis = .horizontal
+        rightStack.alignment = .center
+        rightStack.spacing = 8
+        
+        let rightItems = UIBarButtonItem(customView: rightStack)
+        
+        self.view.backgroundColor = .systemGray4
+        self.navigationItem.rightBarButtonItem = rightItems
+        
+        //MARK: - Back BarItem 설정
+        let backBtnItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
+        backBtnItem.tintColor = .black
+        self.navigationItem.backBarButtonItem = backBtnItem
     }
 }
